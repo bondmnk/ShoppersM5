a1cfefd14bcf2a9dce942535eb11d83ddb9339e2
jsp <jsp@DESKTOP-1S6H73A>
2024-01-06 11:45:17 +0530

    "same modification"

--- src/test/java/TestCases/AddToCart2Test.java
+++ src/test/java/TestCases/AddToCart2Test.java
@@ -15,7 +15,7 @@ public class AddToCart2Test extends BaseClass{
 		System.out.println(Name);
 		System.out.println(Name);
 		System.out.println(Name);
-		System.out.println("vvskjsv lsjv");
-		System.out.println("gslkblskb");
+		System.out.println("vvskjsv lsjvsvbgdsxfhg");
+		System.out.println("gslkblskbfxdfxhhjnf");
 	}
 }
