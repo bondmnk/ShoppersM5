8febc76a82322d1b33c4286195b1536663bdacc2
jsp <jsp@DESKTOP-1S6H73A>
2024-01-06 11:49:33 +0530

    "thrird testcase"

---
+++ src/test/java/TestCases/AddToCart3Test.java
@@ -0,0 +1,15 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class AddToCart3Test {
+
+
+	@Test
+	public void Tc_add_001() {
+		System.out.println("Thrird  testcase");
+		
+	
+	}
+}
+
